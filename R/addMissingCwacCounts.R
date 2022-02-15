51236cde477633f59794db95610c8e26865858cf
patchcervan <francerper@gmail.com>
2022-02-15 15:36:46 +0200

    Added addMissingCwacCounts function

--- R/addMissingCwacCounts.R
+++ R/addMissingCwacCounts.R
@@ -1,6 +1,9 @@
 #' Add missing CWAC counts to CWAC site data
 #'
 #' @param site_counts A dataframe obtained from \link[CWAC]{getCwacSiteCounts}
+#' @param years A vector of years the function should complete. This is useful
+#' for cases where counts are missing at the beginning or end of the period. We
+#' can explicitly state which years should be in the data.
 #'
 #' @return The same site_counts dataframe provided with additional rows for
 #' those Winter or Summer seasons for which no data were collected.
@@ -8,8 +11,8 @@
 #'
 #' @examples
 #' counts <- getCwacSiteCounts(26352535)
-#' counts_w_miss <- addMissingCwacCounts(counts)
-addMissingCwacCounts <- function(site_counts){
+#' counts_w_miss <- addMissingCwacCounts(counts, years = 1993:2020)
+addMissingCwacCounts <- function(site_counts, years){
 
     # Some records are classified as "O" (other). We are only interested in summer and winter
     # so we filter out "O" and sum all counts per card
@@ -18,7 +21,7 @@ addMissingCwacCounts <- function(site_counts){
         dplyr::group_by(Card, Year, Season) %>%
         dplyr::summarize(count = sum(Count)) %>%
         dplyr::ungroup() %>%
-        tidyr::complete(Year = min(Year):max(Year), Season) # Fill in missing years
+        tidyr::complete(Year = years, Season) # Fill in missing years
 
     # Separate missing counts
     missing <- counts_all_spp %>%
